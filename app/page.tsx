3e0869c09554e331ac7b019d3a2293b4ae507171
Arun VK <arunvkazm@gmail.com>
2024-10-27 19:32:00 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,50 @@
+import Layout from "@/components/layout/Layout"
+import Blog2 from "@/components/sections/Blog2"
+import Contact2 from "@/components/sections/Contact2"
+import Coporation2 from "@/components/sections/Coporation2"
+import Education2 from "@/components/sections/Education2"
+import Experience2 from "@/components/sections/Experience2"
+import Home2 from "@/components/sections/Home2"
+import Projects2 from "@/components/sections/Projects2"
+import Service2 from "@/components/sections/Service2"
+import Skills2 from "@/components/sections/Skills2"
+import Static2 from "@/components/sections/Static2"
+import Link from "next/link"
+export default function HomePage2() {
+
+	return (
+		<>
+			<Layout headerStyle={2} footerStyle={2}>
+				<Home2 />
+				<Static2 />
+				<Coporation2 />
+				<Service2 />
+				<Experience2 />
+				<Education2 />
+				<Projects2 />
+				<Skills2 />
+				<Blog2 />
+				<Contact2 />
+			</Layout>
+		</>
+	)
+}
+// export default function Home() {
+
+// 	return (
+// 		<>
+// 			<Layout headerStyle={1} footerStyle={1}>
+// 				<Home1 />
+// 				<Static1 />
+// 				<Service1 />
+// 				<Projects1 />
+// 				<Resume1 />
+// 				<Skills1 />
+// 				<Brands1 />
+// 				<Testimonials1 />
+// 				<Blog1 />
+// 				<Contact1 />
+// 			</Layout>
+// 		</>
+// 	)
+// }
